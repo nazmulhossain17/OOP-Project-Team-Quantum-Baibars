c830d68e9987686106a9c588c2dc0d1ad4fe27d0
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2024-12-03 18:21:34 +0600

    files

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System;
+
+namespace Team_Quantum_Baibars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
