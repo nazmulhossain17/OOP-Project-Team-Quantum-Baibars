d8acb30cd75145debe19c84ffe88380ca087ecb7
jony2411 <maj161715@gmail.com>
2024-12-03 18:52:08 +0600

    update__Customer Class

--- Program.cs
+++ Program.cs
@@ -2,5 +2,12 @@
 {
     public string Name { get; set; }
     public string PhoneNumber { get; set; }
-    public string Role { get; set; } = "customer"; // Default role is "customer"
+    public string Role { get; set; } = "customer";
+
+    public static void AddCustomer(string name, string phoneNumber, List<Customer> customers)
+    {
+        var customer = new Customer { Name = name, PhoneNumber = phoneNumber };
+        customers.Add(customer);
+        Console.WriteLine($"\nCustomer Created: {customer.Name}, Phone: {customer.PhoneNumber}\n");
+    }
 }
